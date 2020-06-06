edcdc4545eae65880c9d1306c7460b13cdb79c1f
jahidul99 <49347044+jahidul99@users.noreply.github.com>
2020-06-06 20:32:50 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import random
+print("########################################################")
+print("           Welcome to Rock Paper Scissor Game           ")
+print("########################################################")
+wrt = open("history.txt", "w")
+count = 0
+options = ["rock", "paper", "scissor"]
+try:
+    while True:
+        print("Enter Your choice or write \"done\" to exit")
+        user_choice = input(">> ")
+        user_choice = user_choice.rstrip()
+        count = count + 1
+        wrt.write(str(count) + "\n")
+        wrt.write("User Choice: " + user_choice + "\n")
+        computer_choice = random.choice(options)
+        wrt.write("Computer Choice: " + computer_choice + "\n")
+        # Rock
+        if user_choice.lower() == "rock":
+            if computer_choice == "scissor":
+                print("Excellent! You Won :)")
+                print("Computer has chosen " + computer_choice)
+
+            elif computer_choice == "paper":
+                print("Sorry you lost :(")
+                print("Computer has chosen " + computer_choice)
+            else:
+                print("Draw")
+                print("Looks like you and computer has same taste.xD")
+        # Paper
+        elif user_choice.lower() == "paper":
+            if computer_choice == "rock":
+                print("Excellent! You Won :)")
+                print("Computer has chosen " + computer_choice)
+            elif computer_choice == "scissor":
+                print("Sorry you lost :(")
+                print("Computer has chosen " + computer_choice)
+            else:
+                print("Draw")
+                print("Looks like you and computer has same taste.xD")
+        # Scissor
+        elif user_choice.lower() == "scissor":
+            if computer_choice == "paper":
+                print("Excellent! You Won :)")
+                print("Computer has chosen " + computer_choice)
+            elif computer_choice == "rock":
+                print("Sorry you lost :(")
+                print("Computer has chosen " + computer_choice)
+            else:
+                print("Draw")
+                print("Looks like you and computer has same taste.xD")
+
+        elif user_choice.lower() == "done":
+            print("Exited from the game")
+            break
+        # I could have used "else" but I tried to experiment with "elif" and "not in" condition
+        elif user_choice.lower() not in options:
+            print("Wrong INPUT. Try again")
+        print("--------------------------------------------------------")
+    wrt.close()
+except:
+    print("Something Went Wrong")
+
